e91a2428eab31d8422237774e3b44deb0770b3d1
wanenaK <wanena.silva@gmail.com>
2019-03-20 11:48:10 -0300

    TUDO ERRADO

---
+++ Classe.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+class Pessoa():
+
+    def __init__():
+        self.lista = lista
+        
+render_template("html.html", usuario=lista)
--- servidor.py
+++ servidor.py
@@ -3,7 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 
+class Pessoa:
+
+    def __init__(self):
+        self.lista = ["Paula", "Rua 8", "3352-5253"
+                     "Tiago", "Beco das Flores", "9998-7071"]
+
 def iniciar():
-    return render_template("html.html")
+    return render_template("html.html", usuario = lista)
 
 app.run(host="0.0.0.0")
