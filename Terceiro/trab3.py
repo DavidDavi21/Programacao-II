f23798a58998e1051f2b1d34b3085a4c640b29bd
Poliana Zalasik <polianaazalasik@outlook.com>
2019-12-04 15:13:13 -0300

    show

--- Terceiro/trab3.py
+++ Terceiro/trab3.py
@@ -8,30 +8,30 @@ class BaseModel(Model):
     class Meta():
         database = db
 
-class Jogador(BaseModel):    
-    nome = CharField()    
-    idade = IntegerField()    
-    tamanho = FloatField()
-
-class Tecnico(BaseModel):    
-    nome = CharField()
-    idade = IntegerField()
-
-class Organizacao(BaseModel):    
-    nome = CharField()    
-    data_evento = DateTimeField()
-
 class Torcedor(BaseModel):    
     nome = CharField()    
     idade = IntegerField()    
     tamanho = FloatField()    
     nacionalidade = CharField()
-    
+
+class Tecnico(BaseModel):    
+    nome = CharField()
+    idade = IntegerField()
+
 class Time(BaseModel):    
-    nome = CharField()    
-    jogadores = ForeignKeyField(Jogador)    
+    nome = CharField()        
     tecnico_do_time = ForeignKeyField(Tecnico)
-    torcedor_do_time = ForeignKeyField(Torcedor)
+    torcedor_do_time = ManyToManyField(Torcedor)
+
+class Jogador(BaseModel):    
+    nome = CharField()    
+    idade = IntegerField()    
+    tamanho = FloatField()
+    time_jogador = ForeignKeyField(Time)
+
+class Organizacao(BaseModel):    
+    nome = CharField()    
+    data_evento = DateTimeField()
     
 class Esporte(BaseModel):    
     nome = CharField()    
@@ -43,13 +43,7 @@ class Arbitro(BaseModel):
     idade = IntegerField()    
     nacionalidade = CharField()
     
-class Partida(BaseModel):    
-    esporte = ForeignKeyField(Esporte)    
-    total_pontos = IntegerField()    
-    jogo = ManyToManyField(Time)    
-    arbitros = ForeignKeyField(Arbitro)
-    
-class Premiacao(BaseModel):    
+class Premiacao(BaseModel):  
     equipe_premiada = ForeignKeyField(Time)    
     jogador_premiado = ManyToManyField(Jogador)
     
@@ -58,31 +52,40 @@ class Campeonato(BaseModel):
     organizacao = ForeignKeyField(Organizacao)
     esporte = ForeignKeyField(Esporte)
     premiacoes = ForeignKeyField(Premiacao)
-    partidas = ForeignKeyField(Partida)
     
+class Partida(BaseModel):    
+    esporte = ForeignKeyField(Esporte)    
+    total_pontos = IntegerField()    
+    jogo = ManyToManyField(Time)    
+    arbitros = ForeignKeyField(Arbitro)
+    campeonato = ForeignKeyField(Campeonato)
+
 if __name__ == "__main__":
     if os.path.exists(arq):
         os.remove(arq)
 
     db.connect()    
-    db.create_tables([Premiacao, Partida, Torcedor, Organizacao, Arbitro, Esporte, Tecnico, Campeonato, Jogador, Time, Campeonato.times.get_through_model(), Esporte.equipes.get_through_model(), Torcedor.time_torcedor.get_through_model(), Partida.jogo.get_through_model(), Premiacao.jogador_premiado.get_through_model()])        
+    db.create_tables([Premiacao, Partida, Torcedor, Organizacao, Arbitro, Esporte, Tecnico, Campeonato, Jogador, Time, Time.torcedor_do_time.get_through_model(), Campeonato.times.get_through_model(), Esporte.equipes.get_through_model(), Partida.jogo.get_through_model(), Premiacao.jogador_premiado.get_through_model()])        
     
     torcedor1 = Torcedor.create(nome="Lucas", idade=28, tamanho=1.63, nacionalidade="Argentino")
-    jogador1 = Jogador.create(nome="João Kleber", idade=16, tamanho=1.75)
     tecnico1 = Tecnico.create(nome="Fausto Silva", idade=45)
-    arbitro1 = Arbitro.create(nome="Gabriel Jesus", idade=17, nacionalidade="Brasileiro")
-    time1 = Time.create(nome="Sao José de Rio Rosa", jogadores=jogador1, tecnico_do_time=tecnico1, torcedor_do_time=torcedor1)
+    time1 = Time.create(nome="Sao José de Rio Rosa", tecnico_do_time=tecnico1)
+    jogador1 = Jogador.create(nome="João Kleber", idade=16, tamanho=1.75, time_jogador=time1)
     organizacao_do_evento = Organizacao.create(nome="CBF", data_evento=12/5/2015)
+    arbitro1 = Arbitro.create(nome="Gabriel Jesus", idade=17, nacionalidade="Brasileiro")
     esporte1 = Esporte.create(nome="Futebol", caracteristicas="bola")
     premiacao_partida = Premiacao.create(equipe_premiada=time1)
-    campeonato1 = Campeonato.create(organizacao=organizacao_do_evento, esporte=esporte1, premiacoes=premiacao_partida)     
-    partida = Partida.create(esporte="Futebol", total_pontos="0/0", arbitros=arbitro1)
+    campeonato1 = Campeonato.create(organizacao=organizacao_do_evento, esporte=esporte1, premiacoes=premiacao_partida)
+    partida = Partida.create(esporte=esporte1, total_pontos="0/0", arbitros=arbitro1, campeonato=campeonato1)     
     
     campeonato1.times.add(time1)    
-    esporte1.equipes.add(time1)    
-    torcedor1.time_torcedor.add(time1)    
+    esporte1.equipes.add(time1)       
     partida.jogo.add(time1)    
     premiacao_partida.jogador_premiado.add(jogador1)
+    time1.torcedor_do_time.add(torcedor1)
+    esporte1.equipes.add(time1)
+    campeonato1.times.add(time1)
+    partida1.jogo.add(time1)
     
     for i in premiacao_partida.jogador_premiado:
         print(i.nome)
--- Terceiro/trab3_backend.py
+++ Terceiro/trab3_backend.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template
 from playhouse.shortcuts import model_to_dict, dict_to_model
 import json
-from jsonpath_ng import jsonpath, parse
 from trab3 import *
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "123"
 
+
+@app.route("/")
+def inicio():
+    pessoas = list(map(model_to_dict, Campeonato.select()))
+    response = jsonify({"lista": pessoas})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+app.run(debug=True)
+
+'''
 campeonatinho = Campeonato.select()
 user_obj = campeonatinho.get()
 json_data = json.dumps(model_to_dict(user_obj, manytomany=True))
 print(json_data)
-
-'''
-@app.route("/")
-def inicio():
-    campeonatinho = Campeonato.select()
-    user_obj = campeonatinho.get()
-    json_data = json.dumps(model_to_dict(user_obj, manytomany=True))
-    return render_template("index.html", dados=json_data)
-
-app.run(debug=True)
 '''
